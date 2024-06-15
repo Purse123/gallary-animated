6d605c1fcf2177caa43e771b20295cb06669536e
Purse123 <sahasneupane85@gmail.com>
2024-06-16 02:20:54 +0545

    finished image animation on gallery

--- src/component/gallery/Link.js
+++ src/component/gallery/Link.js
@@ -1,18 +1,54 @@
-import React, { useRef } from "react";
-import { motion } from "framer-motion";
+import React, { useRef, useEffect } from "react";
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
 
 const Link = (props) => {
   const boxRef = useRef(null);
 
+  const xPosition = useMotionValue(0);
+  const yPosition = useMotionValue(0);
+
+  const xSpring = useSpring(xPosition);
+  const ySpring = useSpring(yPosition);
+
+  const imgTop = useTransform(ySpring, [0.5, -0.5], ["40%", "60%"]);
+  const imgLeft = useTransform(xSpring, [0.5, -0.5], ["60%", "70%"]);
+
   const handleMouseMove = (e) => {
     const rect = boxRef.current.getBoundingClientRect();
-    console.log({rect})
-  }
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    const xPer = mouseX / width - 0.5;
+    const yPer = mouseY / height - 0.5;
+
+    xPosition.set(xPer);
+    yPosition.set(yPer);
+  };
+
+  useEffect(() => {
+    const updateMouseMove = (e) => {
+      if (boxRef.current) {
+        handleMouseMove(e);
+      }
+    };
+    const currentBoxRef = boxRef.current;
+    if (currentBoxRef) {
+      currentBoxRef.addEventListener("mousemove", updateMouseMove);
+    }
+    return () => {
+      if (currentBoxRef) {
+        currentBoxRef.removeEventListener("mousemove", updateMouseMove);
+      }
+    };
+  }, [boxRef]);
 
   return (
     <motion.div
       ref={boxRef}
-      onMouseMove={handleMouseMove}
       initial="hidden"
       whileHover="show"
       className="flex justify-between items-center w-full h-full relative"
@@ -22,9 +58,9 @@ const Link = (props) => {
       </h1>
       <motion.img
         style={{
+          top: imgTop,
+          left: imgLeft,
           position: "absolute",
-          top: "50%",
-          left: "50%",
           translateX: "-50%",
           translateY: "-50%",
         }}
@@ -39,7 +75,7 @@ const Link = (props) => {
         transition={{
           type: "spring",
         }}
-        className=" aspect-video"
+        className="aspect-video z-50 pointer-events-none"
       />
       <p className="montserrat font-normal text-xl uppercase tracking-tighter">
         {props.type}
