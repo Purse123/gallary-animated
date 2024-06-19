938f8a03c4e0baae2dc6da91a103ffa81429769f
Purse123 <sahasneupane85@gmail.com>
2024-06-20 00:19:20 +0545

    added page transition

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import { AnimatePresence } from "framer-motion";
 
-import Navbar from "./component/Navbar";
-import Welcome from "./component/Welcome";
-import { Routes, Route } from 'react-router-dom';
+import Navbar from "./component/hero/Navbar";
+import Welcome from "./component/hero/Welcome";
+import { Routes, Route } from "react-router-dom";
 import Gallery from "./component/gallery/Gallery";
+import Services from "./component/services/Services";
+import Contact from "./component/contact/Contact";
 
 function App() {
   return (
     <div className="h-screen w-full text-white">
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Welcome />}/>
-        <Route path='/gallery' element={<Gallery />}/>
-      </Routes>
+      <Navbar />
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
--- src/component/gallery/Gallery.js
+++ src/component/gallery/Gallery.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { images } from "./imageData";
 import Link from "./Link";
+import transition from "../page_transition/Transition";
+import { motion } from "framer-motion";
 
 const Gallery = () => {
   return (
     <div className="min-h-screen flex flex-col items-center pt-28 w-full bg-neutral-800">
       {images.map((image) => (
-        <div key={image.id} className="h-28 w-4/5 flex items-center justify-center border-b-[1px] border-neutral-50">
+        <motion.div 
+        initial={{
+          opacity: 0,
+          y: "50%"
+        }}
+        animate={{
+          opacity: 1,
+          y: 0
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 10,
+          duration: 2.25,
+          delay: 0.75
+        }}
+        key={image.id} 
+        className="h-28 w-4/5 flex items-center justify-center border-b-[1px] border-neutral-50">
           <Link title={image.title} type={image.catagory} url={image.url}/>
-        </div>
+        </motion.div>
       ))}
     </div>
   );
 };
 
-export default Gallery;
+export default transition(Gallery);
